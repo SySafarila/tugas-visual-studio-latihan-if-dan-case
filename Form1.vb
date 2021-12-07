632eaf010818bc845ad4b3760adefe512e1ca549
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2021-12-07 09:45:00 +0700

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -12,7 +12,6 @@
     Private Sub btnIsiData_Click(sender As Object, e As EventArgs) Handles btnIsiData.Click
         tbTotalHarga.Text = tbHargaBarang.Text * tbJumlahBarang.Text
         tbTotalBayar.Text = tbTotalHarga.Text - tbDiskon.Text
-        MessageBox.Show(caraBayar)
     End Sub
 
     Private Sub rbTunai_CheckedChanged(sender As Object, e As EventArgs) Handles rbTunai.CheckedChanged
@@ -33,5 +32,21 @@
                 tbNamaBarang.Text = "Sandal"
                 tbHargaBarang.Text = 7000
         End Select
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        tbJumlahBarang.Text = ""
+        tbDiskon.Text = ""
+        rbKredit.Checked = False
+        cbKodeBarang.Text = ""
+        tbNamaBarang.Text = ""
+        tbHargaBarang.Text = ""
+        rbTunai.Checked = False
+        tbTotalBayar.Text = ""
+        tbTotalHarga.Text = ""
+    End Sub
+
+    Private Sub btnTutup_Click(sender As Object, e As EventArgs) Handles btnTutup.Click
+        Me.Close()
     End Sub
 End Class
