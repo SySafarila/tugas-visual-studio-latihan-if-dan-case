2be425aafdd75a90225b8539449662a8c00ec1c9
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2021-12-07 09:22:54 +0700

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,5 @@
 ﻿Public Class Form1
+    Dim caraBayar As String = ""
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         cbKodeBarang.Items.Add("001")
         cbKodeBarang.Items.Add("002")
@@ -6,5 +7,14 @@
 
     Private Sub btnIsiData_Click(sender As Object, e As EventArgs) Handles btnIsiData.Click
         tbTotalHarga.Text = tbHargaBarang.Text * tbJumlahBarang.Text
+        MessageBox.Show(caraBayar)
+    End Sub
+
+    Private Sub rbTunai_CheckedChanged(sender As Object, e As EventArgs) Handles rbTunai.CheckedChanged
+        caraBayar = "Tunai"
+    End Sub
+
+    Private Sub rbKredit_CheckedChanged(sender As Object, e As EventArgs) Handles rbKredit.CheckedChanged
+        caraBayar = "Kredit"
     End Sub
 End Class
