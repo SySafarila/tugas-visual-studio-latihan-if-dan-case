a3f3532f79dcd4c185106caca5f9593654b0bb44
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2021-12-07 09:19:10 +0700

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -3,4 +3,8 @@
         cbKodeBarang.Items.Add("001")
         cbKodeBarang.Items.Add("002")
     End Sub
+
+    Private Sub btnIsiData_Click(sender As Object, e As EventArgs) Handles btnIsiData.Click
+        tbTotalHarga.Text = tbHargaBarang.Text * tbJumlahBarang.Text
+    End Sub
 End Class
