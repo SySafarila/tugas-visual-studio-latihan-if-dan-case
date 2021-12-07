357109051fd4e0043464da3fb9fb2f0ce33bf026
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2021-12-07 09:12:49 +0700

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
-    
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cbKodeBarang.Items.Add("001")
+        cbKodeBarang.Items.Add("002")
+    End Sub
 End Class
